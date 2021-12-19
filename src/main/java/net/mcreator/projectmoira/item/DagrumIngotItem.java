f653fe93351906f2a58d9ee56de3da59ba0bd302
Sincitys <Sincitys@DESKTOP-CSMQKRV>
2021-12-19 14:29:16 -0600

    dagrum update 1

---
+++ src/main/java/net/mcreator/projectmoira/item/DagrumIngotItem.java
@@ -0,0 +1,49 @@
+
+package net.mcreator.projectmoira.item;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.item.Rarity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.block.BlockState;
+
+import net.mcreator.projectmoira.ProjectMoiraModElements;
+
+@ProjectMoiraModElements.ModElement.Tag
+public class DagrumIngotItem extends ProjectMoiraModElements.ModElement {
+	@ObjectHolder("project_moira:dagrum_ingot")
+	public static final Item block = null;
+
+	public DagrumIngotItem(ProjectMoiraModElements instance) {
+		super(instance, 1);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new ItemCustom());
+	}
+
+	public static class ItemCustom extends Item {
+		public ItemCustom() {
+			super(new Item.Properties().group(ItemGroup.MISC).maxStackSize(64).rarity(Rarity.COMMON));
+			setRegistryName("dagrum_ingot");
+		}
+
+		@Override
+		public int getItemEnchantability() {
+			return 0;
+		}
+
+		@Override
+		public int getUseDuration(ItemStack itemstack) {
+			return 0;
+		}
+
+		@Override
+		public float getDestroySpeed(ItemStack par1ItemStack, BlockState par2Block) {
+			return 1F;
+		}
+	}
+}
